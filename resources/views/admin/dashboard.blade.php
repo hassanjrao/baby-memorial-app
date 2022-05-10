1a08d09e4db26e8450dacfcc8ee81dc61e32253d
Hassan Javaid Rao <hassanjrao@gmail.com>
2022-05-11 00:21:05 +0400

    resolved bug in tshirt

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,7 +10,7 @@
                         Dashboard
                     </h1>
                     <h2 class="fs-base lh-base fw-medium text-muted mb-0">
-                        Welcome Admin, everything looks great.
+                        Welcome Admin.
                     </h2>
                 </div>
                 <nav class="flex-shrink-0 mt-3 mt-sm-0 ms-sm-3" aria-label="breadcrumb">
@@ -37,15 +37,7 @@
                         <h3 class="block-title">Welcome to Dashboard</h3>
                     </div>
                     <div class="block-content fs-sm text-muted">
-                        <p>
-                            We’ve put everything together, so you can start working on your Laravel project as soon as possible! OneUI assets are integrated and work seamlessly with Laravel Mix, so you can use the npm scripts as you would in any other Laravel project.
-                        </p>
-                        <p>
-                            Feel free to use any examples you like from the full HTML version to build your own pages.
-                        </p>
-                        <p>
-                            <strong>Wish you all the best and happy coding!</strong>
-                        </p>
+                        
                     </div>
                 </div>
             </div>
